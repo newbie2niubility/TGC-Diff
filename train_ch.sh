354deb3d1fecb8c7d76cf2f292a93936395fa15c
Honglie Wang <98510335+newbie2niubility@users.noreply.github.com>
2025-10-21 10:04:49 +0800

    Update feature model path in training script

--- train_ch.sh
+++ train_ch.sh
@@ -2,7 +2,7 @@ export OMP_NUM_THREADS=4
 export TORCH_HOME=./.cache
 
 CUDA_VISIBLE_DEVICES=0, torchrun --nproc_per_node=1 --master_port=14231 train_ch.py \
-    --feat_model ./model_zoo/RN18_class_10400.pth \
+    --feat_model ./.cache/RN18_class_10400.pth \
     --log_name Chinese \
     --cfg ./configs/Chinese_textline_vae16.yaml \
 
